85926dc22b9025f982cf1239ff407a60d39a80c5
HURRAEY <ajvl33333@gmail.com>
2025-11-19 12:02:44 +0900

    cors 설정정

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,29 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:8080', // 로컬 개발 환경
+        'https://an-ai.ai', // 프로덕션 환경
+        'https://www.an-ai.ai', // www 서브도메인
+        'https://dev.an-ai.ai', // 개발/스테이징 환경
+      ],
+      methods: [
+        'GET',
+        'POST',
+        'PUT',
+        'PATCH',
+        'DELETE',
+        'HEAD',
+        'OPTIONS',
+      ],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
